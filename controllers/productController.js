0c307ef36e601d67d755c0e3675a38ba11f9d10e
Salihou Abdoulaye <salehabdallah9910@gmail.com>
2023-08-24 12:17:22 +0200

    add the route for get, put, delete and update product

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,8 +12,44 @@ const allProduct = async (req, res) => {
     }
 }
 
+const createProduct = async (req, res) => {
+    try {
+        const newProduct = await new Product(req.body);
+        newProduct.save();
+        res.json({
+            message: "Congrat ! add new product", 
+            newProduct
+        })
+        
+    } catch (error) {
+        res.json({
+            message: "Error, can't to add product", 
+            errors: error
+        })
+    }
+  };
+
 const updateProduct = async (req, res) => {
-    
+    const id = req.params.product_id 
+    const productData = {
+        name: req.body.name,
+        price: req.body.price,
+        category: req.body.category,
+    };
+    try {
+        const product = await Product.findOneAndUpdate(  {_id: id}, productData)
+        res.send({ msg: "product updated!", product });
+    } catch (error) {
+        res.json({
+            message: "Error, can't update product, try after", 
+            errors: error
+        })
+    }
 }
 
-module.exports = {allProduct}
+const deleteProduct = async (req, res) => {
+    const id = req.params.product_id;
+    await Product.deleteOne({ _id: id });
+    res.send({ msg: " product successfully deleted!" });
+};
+module.exports = {allProduct, updateProduct, createProduct, deleteProduct}
--- routes/produtRoute.js
+++ routes/produtRoute.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
-const {allProduct} = require('../controllers/productController')
+const {allProduct, updateProduct, createProduct, deleteProduct} = require('../controllers/productController')
 
 router.get("/products", allProduct)
+router.put("/product/:product_id", updateProduct)
+router.post("/product/new", createProduct)
+router.delete("/product/:product_id", deleteProduct)
 
 module.exports = router
